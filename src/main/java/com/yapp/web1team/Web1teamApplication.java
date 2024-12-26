4099013c05ee8481f03661fd5d8989b030bb8739
rong5026 <rong5026@naver.com>
2024-12-26 20:56:06 +0900

    :tada: init

---
+++ src/main/java/com/yapp/web1team/Web1teamApplication.java
@@ -0,0 +1,13 @@
+package com.yapp.web1team;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Web1teamApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Web1teamApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/yapp/web1team/Web1teamApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yapp.web1team;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Web1teamApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
