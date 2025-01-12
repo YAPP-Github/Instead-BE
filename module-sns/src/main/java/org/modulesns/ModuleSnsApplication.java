e1bcbf1244601268cd4741a9c94e4022721580c2
rong5026 <rong5026@naver.com>
2025-01-12 18:04:47 +0900

    chore: 멀티모듈 적용

--- module-sns/src/main/java/org/modulesns/ModuleSnsApplication.java
+++ module-sns/src/main/java/org/modulesns/ModuleSnsApplication.java
@@ -1,13 +1,13 @@
-package org.yapp;
+package org.modulesns;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class Application {
+public class ModuleSnsApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(Application.class, args);
+		SpringApplication.run(ModuleSnsApplication.class, args);
 	}
 
 }
