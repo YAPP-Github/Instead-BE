e4ad1dcf730b974d58aa059ea5cde12eebba2205
rong5026 <rong5026@naver.com>
2025-01-12 19:13:35 +0900

    chore: 외부모듈의 등록된 빈을 사용하기 위해 스캔 범위 변경

--- module-sns/src/main/java/org/modulesns/ModuleSnsApplication.java
+++ module-sns/src/main/java/org/modulesns/ModuleSnsApplication.java
@@ -2,8 +2,10 @@ package org.modulesns;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan(basePackages = {"org.modulesns", "org.modulecommon"})
 public class ModuleSnsApplication {
 
 	public static void main(String[] args) {
