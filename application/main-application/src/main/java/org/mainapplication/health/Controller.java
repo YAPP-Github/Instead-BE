7208185e89e45d6725b0fb0e3c75802804b4dc89
rong5026 <rong5026@naver.com>
2025-01-14 18:31:17 +0900

    feat: health check

--- application/main-application/src/main/java/org/mainapplication/health/Controller.java
+++ application/main-application/src/main/java/org/mainapplication/health/Controller.java
@@ -1,12 +1,14 @@
 package org.mainapplication.health;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/yapp")
 public class Controller {
 
-	@GetMapping("/yapp")
+	@GetMapping("/")
 	public String checkHealth() {
 		return "success";
 	}
