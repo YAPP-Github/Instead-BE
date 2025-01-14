c834e72084db1d76b8855a0ec69579a9ad21f7c3
YeongHwan Hong <77156858+rong5026@users.noreply.github.com>
2025-01-14 16:18:23 +0900

    chore: 멀티모듈 패키징 초기세 (#14)

---
+++ domain/domain-module/src/main/java/org/domainmodule/DomainModuleApplication.java
@@ -0,0 +1,13 @@
+package org.domainmodule;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DomainModuleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DomainModuleApplication.class, args);
+	}
+
+}
