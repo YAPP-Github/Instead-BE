deb49ba09f05b4b4437843e1c3c04ad3e7c66264
rong5026 <rong5026@naver.com>
2025-01-20 15:23:45 +0900

    refactor: Ouath의 User LAZY로딩 변경 후 join fetch로 즉시로딩

--- domain/domain-module/src/main/java/org/domainmodule/user/repository/OauthRepository.java
+++ domain/domain-module/src/main/java/org/domainmodule/user/repository/OauthRepository.java
@@ -5,7 +5,10 @@ import java.util.Optional;
 import org.domainmodule.user.entity.Oauth;
 import org.domainmodule.user.entity.type.ProviderType;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface OauthRepository extends JpaRepository<Oauth, Long> {
-	Optional<Oauth> findByProviderAndProviderId(ProviderType provider, String providerId);
+	@Query("SELECT o FROM Oauth o JOIN FETCH o.user WHERE o.provider = :provider AND o.providerId = :providerId")
+	Optional<Oauth> findByProviderAndProviderId(@Param("provider") ProviderType provider, @Param("providerId") String providerId);
 }
