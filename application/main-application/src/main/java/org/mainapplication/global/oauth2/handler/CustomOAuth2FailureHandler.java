df0bbcc0e068d5422133a63928162138a8ab489f
rong5026 <rong5026@naver.com>
2025-01-22 11:49:12 +0900

    refactor: oauth2 응답값 처리 재설정
    - oauth2 로그인 실패시 서비스 url로 리다이렉트
    - 예외발생 시 globalResponse로 에러문구 리턴

--- application/main-application/src/main/java/org/mainapplication/global/oauth2/handler/CustomOAuth2FailureHandler.java
+++ application/main-application/src/main/java/org/mainapplication/global/oauth2/handler/CustomOAuth2FailureHandler.java
@@ -2,10 +2,16 @@ package org.mainapplication.global.oauth2.handler;
 
 import java.io.IOException;
 
+import org.mainapplication.global.constants.UrlConstants;
+import org.mainapplication.global.error.ErrorResponse;
+import org.mainapplication.global.response.GlobalResponse;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.web.authentication.AuthenticationFailureHandler;
 import org.springframework.stereotype.Component;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
@@ -14,11 +20,9 @@ public class CustomOAuth2FailureHandler implements AuthenticationFailureHandler 
 
 	@Override
 	public void onAuthenticationFailure(HttpServletRequest request, HttpServletResponse response, AuthenticationException exception) throws
-		IOException {
+		IOException, ServletException {
 
-		// 실패 응답을 JSON으로 반환
-		response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
-		response.setContentType("application/json;charset=UTF-8");
-		response.getWriter().write("{\"error\": \"Authentication failed\", \"message\": \"" + exception.getMessage() + "\"}");
+		//TODO 서비스 페이지로 리다이렉트할지, Error Response를 리턴할지 추후에 결정
+		response.sendRedirect(UrlConstants.PROD_DOMAIN_URL + "?error=" + exception.getMessage());
 	}
 }
