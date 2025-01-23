5ee0e1d7532605f88eb7ab2c89af6a80f7d4085e
rong5026 <rong5026@naver.com>
2025-01-23 16:16:16 +0900

    chore: main-module에서 외부 yml 파일 가져올 수 있도록 설정

--- application/main-application/src/main/java/org/mainapplication/MainApplication.java
+++ application/main-application/src/main/java/org/mainapplication/MainApplication.java
@@ -5,10 +5,11 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
-@ComponentScan(basePackages = {"org.mainapplication", "org.domainmodule"})
+@ComponentScan(basePackages = {"org.mainapplication", "org.domainmodule", "org.snsclient"})
 public class MainApplication {
 
 	public static void main(String[] args) {
+		System.setProperty("spring.config.name", "application,application-domain,application-sns");
 		SpringApplication.run(MainApplication.class, args);
 	}
 
