222836bcd8b545fd2c4e17a5772a85b88e3ff963
rong5026 <rong5026@naver.com>
2025-01-23 16:17:54 +0900

    feat: x 로그인 요청
     - 1. code 받급
     - 2. AccessToken, RefreshToken 요청
     - 3. DB에 저장

---
+++ application/main-application/src/main/java/org/mainapplication/domain/sns/twitter/TwitterService.java
@@ -0,0 +1,48 @@
+package org.mainapplication.domain.sns.twitter;
+
+import java.net.URI;
+
+import org.domainmodule.agent.entity.Agent;
+import org.mainapplication.domain.agent.service.AgentService;
+import org.mainapplication.domain.sns.token.SnsTokenService;
+import org.snsclient.twitter.dto.response.TwitterTokenResponse;
+import org.snsclient.twitter.dto.response.TwitterUserInfoDto;
+import org.snsclient.twitter.service.TwitterApiService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+
+import jakarta.transaction.Transactional;
+import lombok.RequiredArgsConstructor;
+
+@Service
+@RequiredArgsConstructor
+public class TwitterService {
+	private final TwitterApiService twitterApiService;
+	private final AgentService agentService;
+	private final SnsTokenService snsTokenService;
+
+	/**
+	 * Twitter Authorization URL 생성 및 리다이렉트 ResponseEntity 반환
+	 */
+	public ResponseEntity<Void> createRedirectResponse() {
+		// 리다이렉트 URL 생성
+		String redirectUrl = twitterApiService.getTwitterAuthorizationUrl();
+		return ResponseEntity.status(HttpStatus.SEE_OTHER)
+			.location(URI.create(redirectUrl))
+			.build();
+	}
+
+	/**
+	 * Authorization Code를 처리하여 토큰 발급
+	 * 사용자 계정(Agent)과 SNS 토큰(SnsToken)을 저장
+	 */
+	@Transactional
+	public void loginOrRegister(String code) {
+		TwitterTokenResponse tokenResponse = twitterApiService.getTwitterAuthorizationToken(code);
+		TwitterUserInfoDto userInfo = twitterApiService.getUserInfo(tokenResponse.accessToken(), tokenResponse.refreshToken());
+
+		Agent agent = agentService.findOrCreateAgent(userInfo);
+		snsTokenService.createOrUpdateSnsToken(agent, tokenResponse);
+	}
+}
