791e1e49dafa4ed094eedef4b9c95f1912e5699b
rong5026 <rong5026@naver.com>
2025-01-25 13:42:28 +0900

    refactor: User와 SnsToken 양방향 매핑
    - Post 업로드 시 User의 정보로 SnsToken에 편하게 접근할 수 있도록 설정

--- domain/domain-module/src/main/java/org/domainmodule/user/entity/User.java
+++ domain/domain-module/src/main/java/org/domainmodule/user/entity/User.java
@@ -1,12 +1,15 @@
 package org.domainmodule.user.entity;
 
 import org.domainmodule.common.entity.BaseAuditEntity;
+import org.domainmodule.snstoken.entity.SnsToken;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToOne;
 import lombok.AccessLevel;
 import lombok.Builder;
 import lombok.Getter;
@@ -27,6 +30,9 @@ public class User extends BaseAuditEntity {
 
 	@Column(nullable = false, unique = true, length = 320)
 	private String email;
+
+	@OneToOne(mappedBy = "agent", fetch = FetchType.LAZY)
+	private SnsToken snsToken;
 
 	@Builder
 	private User (
