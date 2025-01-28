25fb257b596adb2e11c2bea8318ac07f44a4a5e4
rong5026 <rong5026@naver.com>
2025-01-28 16:34:40 +0900

    refactor: 전역 응답값 Success: boolean 필드 제거

--- application/main-application/src/main/java/org/mainapplication/global/response/GlobalResponse.java
+++ application/main-application/src/main/java/org/mainapplication/global/response/GlobalResponse.java
@@ -4,12 +4,12 @@ import java.time.LocalDateTime;
 
 import org.mainapplication.global.error.ErrorResponse;
 
-public record GlobalResponse(boolean success, int status, Object data, LocalDateTime timestamp) {
+public record GlobalResponse(int status, Object data, LocalDateTime timestamp) {
     public static GlobalResponse success(int status, Object data) {
-        return new GlobalResponse(true, status, data, LocalDateTime.now());
+        return new GlobalResponse(status, data, LocalDateTime.now());
     }
 
     public static GlobalResponse fail(int status, ErrorResponse errorResponse) {
-        return new GlobalResponse(false, status, errorResponse, LocalDateTime.now());
+        return new GlobalResponse(status, errorResponse, LocalDateTime.now());
     }
 }
