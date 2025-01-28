daa37c733b1a40ca0557f3d4e812b2fd7694459a
y-ngm-n <wogitopp@naver.com>
2025-01-29 01:14:56 +0900

    feat: PostService에 예외처리 추가

--- application/main-application/src/main/java/org/mainapplication/domain/post/service/PostService.java
+++ application/main-application/src/main/java/org/mainapplication/domain/post/service/PostService.java
@@ -6,6 +6,7 @@ import java.util.concurrent.CompletableFuture;
 import org.domainmodule.post.entity.Post;
 import org.domainmodule.post.entity.type.PostStatusType;
 import org.domainmodule.postgroup.entity.PostGroup;
+import org.domainmodule.postgroup.entity.PostGroupImage;
 import org.domainmodule.rssfeed.entity.RssFeed;
 import org.domainmodule.rssfeed.repository.RssFeedRepository;
 import org.feedclient.service.FeedService;
@@ -13,6 +14,7 @@ import org.feedclient.service.dto.FeedPagingResult;
 import org.mainapplication.domain.post.controller.request.CreatePostsRequest;
 import org.mainapplication.domain.post.controller.response.CreatePostsResponse;
 import org.mainapplication.domain.post.controller.response.type.PostResponse;
+import org.mainapplication.domain.post.exception.PostGenerateFailedException;
 import org.mainapplication.domain.post.service.dto.SavePostGroupAndPostsDto;
 import org.mainapplication.domain.post.service.dto.SavePostGroupWithImagesAndPostsDto;
 import org.mainapplication.openai.contentformat.jsonschema.SummaryContentSchema;
@@ -58,7 +60,7 @@ public class PostService {
 			summaryContentSchema.getResponseFormat(), limit, null)
 			.addDeveloperMessage(instructionPrompt)
 			.addUserTextMessage(topicPrompt);
-		ChatCompletionResponse result = openAiClient.getChatCompletion(chatCompletionRequest);
+		ChatCompletionResponse result = generatePosts(chatCompletionRequest);
 
 		// PostGroup 엔티티 생성
 		PostGroup postGroup = PostGroup.createPostGroup(null, null, request.getTopic(), request.getPurpose(),
@@ -67,15 +69,9 @@ public class PostService {
 		// Post 엔티티 생성: OpenAI API 응답의 choices에서 답변 꺼내 json으로 파싱 후 엔티티 생성
 		List<Post> posts = result.getChoices().stream()
 			.map(choice -> {
-				try {
-					SummaryContentFormat content = objectMapper.readValue(choice.getMessage().getContent(),
-						SummaryContentFormat.class);
-					return Post.createPost(postGroup, null, content.getSummary(), content.getContent(),
-						PostStatusType.GENERATED, null);
-				} catch (JsonProcessingException e) {
-					return Post.createPost(postGroup, null, "게시물", choice.getMessage().getContent(),
-						PostStatusType.GENERATED, null);
-				}
+				SummaryContentFormat content = parseSummaryContentFormat(choice.getMessage().getContent());
+				return Post.createPost(postGroup, null, content.getSummary(), content.getContent(),
+					PostStatusType.GENERATED, null);
 			})
 			.toList();
 
@@ -107,7 +103,7 @@ public class PostService {
 
 		// 게시물 생성하기: 각 뉴스 기사별로 OpenAI API 호출 및 답변 생성
 		List<CompletableFuture<ChatCompletionResponse>> resultFutures = refPrompts.stream()
-			.map(refPrompt -> openAiClient.getChatCompletionAsync(
+			.map(refPrompt -> generatePostsAsync(
 				new ChatCompletionRequest(openAiModel, summaryContentSchema.getResponseFormat(), null, null)
 					.addDeveloperMessage(instructionPrompt)
 					.addUserTextMessage(topicPrompt)
@@ -125,16 +121,10 @@ public class PostService {
 		// Post 엔티티 생성
 		List<Post> posts = results.stream()
 			.map(result -> {
-				try {
-					SummaryContentFormat content = objectMapper.readValue(
-						result.getChoices().get(0).getMessage().getContent(), SummaryContentFormat.class);
-					return Post.createPost(postGroup, null, content.getSummary(), content.getContent(),
-						PostStatusType.GENERATED, null);
-				} catch (JsonProcessingException e) {
-					return Post.createPost(postGroup, null, "게시물",
-						result.getChoices().get(0).getMessage().getContent(),
-						PostStatusType.GENERATED, null);
-				}
+				SummaryContentFormat content = parseSummaryContentFormat(
+					result.getChoices().get(0).getMessage().getContent());
+				return Post.createPost(postGroup, null, content.getSummary(), content.getContent(),
+					PostStatusType.GENERATED, null);
 			})
 			.toList();
 
@@ -157,52 +147,76 @@ public class PostService {
 		String topicPrompt = createPostPrompt.getBasicTopicPrompt(request);
 		String imageRefPrompt = createPostPrompt.getImageRefPrompt();
 
-		// 이미지 인코딩
-		// List<String> encodedImages = request.getImages().stream()
-		// 	.map(image -> {
-		// 		try {
-		// 			return Base64.getEncoder().encodeToString(image.getBytes());
-		// 		} catch (IOException e) {
-		// 			return null;
-		// 		}
-		// 	})
-		// 	.toList();
-
 		// 게시물 생성
-		ChatCompletionResponse result = openAiClient.getChatCompletion(
-			new ChatCompletionRequest(openAiModel, summaryContentSchema.getResponseFormat(), limit, null)
-				.addDeveloperMessage(instructionPrompt)
-				.addUserTextMessage(topicPrompt)
-				.addUserImageMessage(imageRefPrompt, request.getImages())
-		);
+		ChatCompletionRequest chatCompletionRequest = new ChatCompletionRequest(openAiModel,
+			summaryContentSchema.getResponseFormat(), limit, null)
+			.addDeveloperMessage(instructionPrompt)
+			.addUserTextMessage(topicPrompt)
+			.addUserImageMessage(imageRefPrompt, request.getImageUrls());
+		ChatCompletionResponse result = generatePosts(chatCompletionRequest);
 
 		// PostGroup 엔티티 생성
 		PostGroup postGroup = PostGroup.createPostGroup(null, null, request.getTopic(), request.getPurpose(),
 			request.getReference(), request.getLength(), request.getContent());
 
+		// PostGroupImage 엔티티 리스트 생성
+		List<PostGroupImage> postGroupImages = request.getImageUrls().stream()
+			.map(imageUrl -> PostGroupImage.createPostGroupImage(postGroup, imageUrl))
+			.toList();
+
 		// Post 엔티티 리스트 생성
 		List<Post> posts = result.getChoices().stream()
 			.map(choice -> {
-				try {
-					SummaryContentFormat content = objectMapper.readValue(choice.getMessage().getContent(),
-						SummaryContentFormat.class);
-					return Post.createPost(postGroup, null, content.getSummary(), content.getContent(),
-						PostStatusType.GENERATED, null);
-				} catch (JsonProcessingException e) {
-					return Post.createPost(postGroup, null, "게시물", choice.getMessage().getContent(),
-						PostStatusType.GENERATED, null);
-				}
+				SummaryContentFormat content = parseSummaryContentFormat(choice.getMessage().getContent());
+				return Post.createPost(postGroup, null, content.getSummary(), content.getContent(),
+					PostStatusType.GENERATED, null);
 			})
 			.toList();
 
 		// 엔티티 저장
 		SavePostGroupWithImagesAndPostsDto saveResult = postTransactionService.savePostGroupWithImagesAndPosts(
-			postGroup, null, posts);
+			postGroup, postGroupImages, posts);
 
 		// 결과 반환
 		List<PostResponse> postResponses = saveResult.posts().stream()
 			.map(PostResponse::from)
 			.toList();
 		return new CreatePostsResponse(saveResult.postGroup().getId(), null, postResponses);
+	}
+
+	/**
+	 * OpenAiClient의 getChatCompletion 메서드를 호출하여 게시물 생성을 요청하는 메서드
+	 * 예외 발생 시 PostGenerateFailedException 발생
+	 */
+	private ChatCompletionResponse generatePosts(ChatCompletionRequest request) {
+		try {
+			return openAiClient.getChatCompletion(request);
+		} catch (RuntimeException e) {
+			throw new PostGenerateFailedException();
+		}
+	}
+
+	/**
+	 * OpenAiClient의 getChatCompletionAsync 메서드를 호출하여 게시물 생성을 요청하는 메서드
+	 * 예외 발생 시 PostGenerateFailedException 발생
+	 */
+	private CompletableFuture<ChatCompletionResponse> generatePostsAsync(ChatCompletionRequest request) {
+		try {
+			return openAiClient.getChatCompletionAsync(request);
+		} catch (RuntimeException e) {
+			throw new PostGenerateFailedException();
+		}
+	}
+
+	/**
+	 * OpenAI API의 응답 content를 SummaryContentFormat 형태로 파싱하는 메서드
+	 * 파싱에 실패한 경우 대체 content를 반환
+	 */
+	private SummaryContentFormat parseSummaryContentFormat(String content) {
+		try {
+			return objectMapper.readValue(content, SummaryContentFormat.class);
+		} catch (JsonProcessingException e) {
+			return SummaryContentFormat.createAlternativeFormat("생성된 게시물", content);
+		}
 	}
 }
--- application/main-application/src/main/java/org/mainapplication/openai/contentformat/response/SummaryContentFormat.java
+++ application/main-application/src/main/java/org/mainapplication/openai/contentformat/response/SummaryContentFormat.java
@@ -1,11 +1,18 @@
 package org.mainapplication.openai.contentformat.response;
 
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 
 @Getter
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
 public class SummaryContentFormat {
 
 	private String summary;
 
 	private String content;
+
+	public static SummaryContentFormat createAlternativeFormat(String summary, String content) {
+		return new SummaryContentFormat(summary, content);
+	}
 }
