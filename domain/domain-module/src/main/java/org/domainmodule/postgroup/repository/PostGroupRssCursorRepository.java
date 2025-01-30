bb8b8fb376cbaff329ad9492223c37382c42e16f
y-ngm-n <wogitopp@naver.com>
2025-01-30 22:53:28 +0900

    feat: PostGroupRssCursorRepository 구현

---
+++ domain/domain-module/src/main/java/org/domainmodule/postgroup/repository/PostGroupRssCursorRepository.java
@@ -0,0 +1,7 @@
+package org.domainmodule.postgroup.repository;
+
+import org.domainmodule.postgroup.entity.PostGroupRssCursor;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PostGroupRssCursorRepository extends JpaRepository<PostGroupRssCursor, Long> {
+}
