0356b94b096abbe8a380ae518d0c7b355628799c
y-ngm-n <wogitopp@naver.com>
2025-02-01 04:54:23 +0900

    feat: 게시물 개별 일반 삭제 API 요청 객체 구현

---
+++ application/main-application/src/main/java/org/mainapplication/domain/post/controller/request/UpdatePostRequest.java
@@ -0,0 +1,32 @@
+package org.mainapplication.domain.post.controller.request;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+import org.domainmodule.post.entity.type.PostStatusType;
+import org.mainapplication.domain.post.controller.request.type.UpdatePostType;
+import org.springframework.lang.Nullable;
+
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class UpdatePostRequest {
+
+	@NotNull(message = "수정 타입을 지정해주세요.")
+	private UpdatePostType updateType;
+
+	@Nullable
+	private PostStatusType status;
+
+	@Nullable
+	private LocalDateTime uploadTime;
+
+	@Nullable
+	private String content;
+
+	@Nullable
+	private List<String> imageUrls;
+}
---
+++ application/main-application/src/main/java/org/mainapplication/domain/post/controller/request/type/UpdatePostType.java
@@ -0,0 +1,5 @@
+package org.mainapplication.domain.post.controller.request.type;
+
+public enum UpdatePostType {
+	STATUS, UPLOAD_TIME, CONTENT, CONTENT_WITH_IMAGE
+}
