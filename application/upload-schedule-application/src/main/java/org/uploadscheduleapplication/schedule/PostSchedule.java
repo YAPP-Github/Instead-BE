6008f7486baae4dcf33954dfa77da3d80f0f7bb5
rong5026 <rong5026@naver.com>
2025-01-31 17:41:14 +0900

    feature: 스케쥴링 안돌도록 주석처리

--- application/upload-schedule-application/src/main/java/org/uploadscheduleapplication/schedule/PostSchedule.java
+++ application/upload-schedule-application/src/main/java/org/uploadscheduleapplication/schedule/PostSchedule.java
@@ -14,7 +14,7 @@ public class PostSchedule {
 	 * 1분마다 실행되는 스케줄러
 	 */
 	//TODO SnsToken의 만료시간 계산 후 재발급 로직 추가해야함
-	@Scheduled(cron = "0 */1 * * * *")
+	// @Scheduled(cron = "0 */1 * * * *")
 	public void startUploadPost() {
 		uploadPostService.uploadPosts();
 	}
