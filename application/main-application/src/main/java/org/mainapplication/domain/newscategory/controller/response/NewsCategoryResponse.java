3ff53fbc9eb5d8275ae4eea64f76a9abbc51c0e9
y-ngm-n <wogitopp@naver.com>
2025-02-02 02:10:41 +0900

    docs: 뉴스 카테고리 목록 조회 API 문서 작성

--- application/main-application/src/main/java/org/mainapplication/domain/newscategory/controller/response/NewsCategoryResponse.java
+++ application/main-application/src/main/java/org/mainapplication/domain/newscategory/controller/response/NewsCategoryResponse.java
@@ -3,8 +3,13 @@ package org.mainapplication.domain.newscategory.controller.response;
 import org.domainmodule.rssfeed.entity.RssFeed;
 import org.domainmodule.rssfeed.entity.type.FeedCategoryType;
 
+import io.swagger.v3.oas.annotations.media.Schema;
+
+@Schema(description = "뉴스 카테고리 목록 조회 API 응답 본문")
 public record NewsCategoryResponse(
+	@Schema(description = "코드 레벨에서 카테고리를 구분하는 Enum", example = "INVEST")
 	FeedCategoryType category,
+	@Schema(description = "화면에 표시될 카테고리 한글명", example = "투자")
 	String name
 ) {
 
