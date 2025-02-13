12b0c6f7d0a5aaf92d2bd8e33fd9fe098c075e34
rong5026 <rong5026@naver.com>
2025-02-13 14:23:33 +0900

    fix: JwtProperties 토큰 만료값, 시크릿값 못가져오는 에러 수정

--- application/main-app/src/main/java/org/mainapp/global/constants/JwtProperties.java
+++ application/main-app/src/main/java/org/mainapp/global/constants/JwtProperties.java
@@ -1,6 +1,7 @@
 package org.mainapp.global.constants;
 
 import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 import org.springframework.stereotype.Component;
 
 import lombok.Getter;
@@ -9,10 +10,11 @@ import lombok.Setter;
 @Getter
 @Setter
 @Component
+@ConfigurationPropertiesScan
 @ConfigurationProperties(prefix = "jwt")
 public class JwtProperties {
-	private long accessTokenExpirationMS;
-	private long refreshTokenExpirationMS;
+	private long accessTokenExpirationMs;
+	private long refreshTokenExpirationMs;
 	private String accessTokenKey;
 	private String refreshTokenKey;
 }
