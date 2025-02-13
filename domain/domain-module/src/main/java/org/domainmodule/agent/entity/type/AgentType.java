6f3078842b2f0e9601b0d13d4addc39b065d815e
rong5026 <rong5026@naver.com>
2025-02-13 14:56:10 +0900

    refactor: AgentType종류 FREE, BASIC, PREMIUM, PREMIUM_PLUS 4가지로 지정

--- domain/domain-module/src/main/java/org/domainmodule/agent/entity/type/AgentType.java
+++ domain/domain-module/src/main/java/org/domainmodule/agent/entity/type/AgentType.java
@@ -1,14 +1,16 @@
 package org.domainmodule.agent.entity.type;
 
 public enum AgentType {
-	BASIC, PREMIUM;
+	FREE, BASIC, PREMIUM, PREMIUM_PLUS ;
 	public static AgentType fromSubscription(String subscriptionType) {
 		if (subscriptionType == null) {
-			return BASIC;
+			return FREE;
 		}
 		return switch (subscriptionType.toUpperCase()) {
+			case "BASIC" -> BASIC;
 			case "PREMIUM" -> PREMIUM;
-			default -> BASIC;
+			case "PREMIUMPLUS" -> PREMIUM_PLUS;
+			default -> FREE;
 		};
 	}
 }
