f5faecf4dd7a2069c3aefaa9bcfa1ba3a396ff18
y-ngm-n <wogitopp@naver.com>
2025-02-21 03:26:33 +0900

    feat: 계정별 예약 게시물 예약일시 수정 API 구현

--- application/main-app/src/main/java/org/mainapp/domain/post/controller/request/UpdateReservedPostsRequest.java
+++ application/main-app/src/main/java/org/mainapp/domain/post/controller/request/UpdateReservedPostsRequest.java
@@ -1,20 +1,18 @@
 package org.mainapp.domain.post.controller.request;
 
-import java.time.LocalDateTime;
+import java.util.List;
+
+import org.mainapp.domain.post.controller.request.type.UpdateReservedPostsRequestItem;
 
 import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.validation.Valid;
 import jakarta.validation.constraints.NotNull;
 
 @Schema(description = "계정별 예약 게시물 예약일시 수정 API 요청 본문")
 public record UpdateReservedPostsRequest(
-	@Schema(description = "게시물 id", example = "1")
-	@NotNull(message = "게시물 id를 지정해주세요.")
-	Long postId,
-	@Schema(description = "게시물 그룹 id", example = "1")
-	@NotNull(message = "게시물 그룹 id를 지정해주세요.")
-	Long postGroupId,
-	@Schema(description = "변경할 업로드 예약일시", example = "2025-01-01T00:00:00.000Z")
-	@NotNull(message = "업로드 예약일시를 지정해주세요.")
-	LocalDateTime uploadTime
+	@Schema(description = "게시물 예약일시 수정 정보 리스트")
+	@NotNull(message = "게시물 예약일시 수정 정보를 입력해주세요.")
+	@Valid
+	List<UpdateReservedPostsRequestItem> posts
 ) {
 }
