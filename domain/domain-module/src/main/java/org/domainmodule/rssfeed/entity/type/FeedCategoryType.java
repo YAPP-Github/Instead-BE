e6285adbcba0a8efd7b3b8254de494eb4908bcd9
y-ngm-n <wogitopp@naver.com>
2025-02-22 01:24:24 +0900

    feat: 뉴스 카테고리 타입 추가

--- domain/domain-module/src/main/java/org/domainmodule/rssfeed/entity/type/FeedCategoryType.java
+++ domain/domain-module/src/main/java/org/domainmodule/rssfeed/entity/type/FeedCategoryType.java
@@ -6,28 +6,30 @@ import lombok.Getter;
 @Getter
 @AllArgsConstructor
 public enum FeedCategoryType {
-	INVEST("투자"), // 제거
-	STOCK("주식"), // 제거
+	STOCK("주식"),
 	REALESTATE("부동산"),
-	FASHION("패션"),
-	TRAVEL("여행"),
-	BEAUTY("뷰티"), // 제거
-	FITNESS("피트니스"), // 제거
-	COOKING("요리"), // 제거
-	HEALTHCARE("헬스케어"),
-	AI("AI"),
-	GAME("게임"), // 제거
-	APP("앱"), // 제거
-	SPACE("우주"),
-	ENVIRONMENT("환경"),
-	ENGINEER("공학"),
 	WEATHER("날씨"), // 추가
+	TRAVEL("여행"),
+	HEALTHCARE("건강"),
 	FOOD("음식"), // 추가
 	EDUCATION("교육"), // 추가
 	ACCIDENT("사건사고"), // 추가
-	STOCK_DOMESTIC("주식 (국내 뉴스)"), // 추가
-	STOCK_GLOBAL("주식 (해외 뉴스)"), // 추가
-	FINANCE("금융"); // 추가
+	AI("AI"),
+	GAME("게임"),
+	BASEBALL_KOREA("국내 야구"), // 추가
+	BASEBALL_GLOBAL("해외 야구"), // 추가
+	FOOTBALL_GLOBAL("해외 축구"), // 추가
+	GOLF("골프"), // 추가
+	ESPORTS("E-스포츠"), // 추가
+	FASHION("패션"),
+	BEAUTY("뷰티"),
+	INVEST("투자"),
+	FITNESS("피트니스"),
+	COOKING("요리"),
+	APP("앱"),
+	SPACE("우주"),
+	ENVIRONMENT("환경"),
+	ENGINEER("공학");
 
 	private final String value;
 }
