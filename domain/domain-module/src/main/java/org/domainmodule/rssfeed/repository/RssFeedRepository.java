1f02b1b026795fd496e857bc6f923f2cbd99a3d5
y-ngm-n <wogitopp@naver.com>
2025-02-22 01:18:49 +0900

    feat: 뉴스 카테고리 목록 비활성화 로직 추가

--- domain/domain-module/src/main/java/org/domainmodule/rssfeed/repository/RssFeedRepository.java
+++ domain/domain-module/src/main/java/org/domainmodule/rssfeed/repository/RssFeedRepository.java
@@ -1,12 +1,20 @@
 package org.domainmodule.rssfeed.repository;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.domainmodule.rssfeed.entity.RssFeed;
 import org.domainmodule.rssfeed.entity.type.FeedCategoryType;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 public interface RssFeedRepository extends JpaRepository<RssFeed, Long> {
 
 	Optional<RssFeed> findByCategory(FeedCategoryType category);
+
+	@Query("""
+			select rf from RssFeed rf
+			where rf.isActivated = true
+		""")
+	List<RssFeed> findAllActivated();
 }
