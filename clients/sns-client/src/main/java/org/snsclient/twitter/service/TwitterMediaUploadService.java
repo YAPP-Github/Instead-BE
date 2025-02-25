9377347cb17918b276e3e6760e773943b212ff95
rong5026 <rong5026@naver.com>
2025-02-25 21:00:48 +0900

    refactor: media upload 메서드 분리

--- clients/sns-client/src/main/java/org/snsclient/twitter/service/TwitterMediaUploadService.java
+++ clients/sns-client/src/main/java/org/snsclient/twitter/service/TwitterMediaUploadService.java
@@ -34,7 +34,13 @@ public class TwitterMediaUploadService {
 	public String uploadMedia(String presignedUrl, String accessToken) throws TwitterException {
 		// 이미지 다운로드
 		byte[] imageBytes = imageDownloadClient.downloadImage(presignedUrl);
+		// 이미지 업로드
+		String uploadResponse = uploadImageToTwitter(imageBytes, accessToken);
+		// Media Id 추출
+		return extractMediaId(uploadResponse);
+	}
 
+	private String uploadImageToTwitter(byte[] imageBytes, String accessToken) throws TwitterException {
 		// INIT 요청 (업로드 준비 요청)
 		String initResponse = initUpload(imageBytes.length, accessToken);
 		String mediaId = extractMediaId(initResponse);
@@ -43,9 +49,9 @@ public class TwitterMediaUploadService {
 		appendMedia(mediaId, imageBytes, accessToken);
 
 		// FINALIZE 요청
-		String finalizeResponse = finalizeUpload(mediaId, accessToken);
-		return extractMediaId(finalizeResponse);
+		return finalizeUpload(mediaId, accessToken);
 	}
+
 
 	/**
 	 * INIT 요청 (미디어 업로드 세션 생성)
@@ -66,7 +72,7 @@ public class TwitterMediaUploadService {
 		MultipartBodyBuilder builder = new MultipartBodyBuilder();
 		builder.part("command", "APPEND");
 		builder.part("media_id", mediaId);
-		builder.part("segment_index", "0");
+		builder.part("segment_index", "0"); // chunk upload시 index
 		builder.part("media", new ByteArrayResource(imageBytes) {
 			@Override
 			public String getFilename() {
