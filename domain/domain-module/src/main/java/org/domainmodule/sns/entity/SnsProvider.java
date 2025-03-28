40a6ca453e71ddafdada506e216fbcec521a36da
rong5026 <rong5026@naver.com>
2025-03-28 16:04:47 +0900

    feature: SnsProvider 엔티티 생성

---
+++ domain/domain-module/src/main/java/org/domainmodule/sns/entity/SnsProvider.java
@@ -0,0 +1,37 @@
+package org.domainmodule.sns.entity;
+
+import org.domainmodule.common.entity.BaseTimeEntity;
+import org.domainmodule.user.entity.User;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class SnsProvider extends BaseTimeEntity {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "sns_provider")
+	private Long id;
+
+	@Column(nullable = false, unique = true)
+	private String clientId;
+
+	@Column(nullable = false, unique = true)
+	private String clientSecret;
+
+	@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "user_id", nullable = false)
+	private User user;
+}
---
+++ domain/domain-module/src/main/java/org/domainmodule/sns/repository/SnsProviderRespository.java
@@ -0,0 +1,7 @@
+package org.domainmodule.sns.repository;
+
+import org.domainmodule.sns.entity.SnsProvider;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface SnsProviderRespository extends JpaRepository<SnsProvider, Long> {
+}
